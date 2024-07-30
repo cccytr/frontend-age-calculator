c2aba4029957d1771ba72a7da370e6e4aeef592c
ccc <ucandusso@gmail.com>
2024-07-30 05:24:51 -0300

    age calculator

---
+++ index.js
@@ -0,0 +1,86 @@
+const input_year = document.getElementById('year');
+const input_month = document.getElementById('month');
+const input_day = document.getElementById('day');
+
+const output_year = document.getElementById("years");
+const output_months = document.getElementById("months");
+const output_days = document.getElementById("days");
+
+const error_year = document.querySelector(".error-year");
+const error_month = document.querySelector(".error-month");
+const error_day = document.querySelector(".error-day");
+
+const submitBtn = document.querySelector(".submit-btn");
+
+let isValid = false;
+submitBtn.addEventListener('click', calculateAge);
+
+input_day.addEventListener('input', e => {
+    if (input_day.value > 31) {
+        error_day.textContent = "Must be a valid day";
+        isValid;
+        return;
+    } else {
+        isValid = true;
+        error_day.textContent = '';
+    }
+    if (+input_day.value === 0) {
+        error_day.textContent = "This field is required";
+        isValid;
+        return;
+    }
+});
+
+
+input_month.addEventListener('input', e => {
+    if (input_month.value > 12) {
+        error_month.textContent = "Must be a valid month";
+        isValid;
+        return;
+    } else {
+        isValid = true;
+        error_month.textContent = '';
+    }
+    if (+input_month.value === 0) {
+        error_month.textContent = "This field is required";
+        isValid;
+        return;
+    }
+});
+
+
+input_year.addEventListener('input', e => {
+    if (input_year.value > 2024) {
+        error_year.textContent = "Must be a valid year";
+        isValid;
+        return;
+    } else {
+        isValid = true;
+        error_year.textContent = '';
+    }
+    if (+input_year.value === 0) {
+        error_year.textContent = "This field is required";
+        isValid;
+        return;
+    }
+});
+
+
+function calculateAge() {
+    const day = document.getElementById('day').value;
+    const month = document.getElementById('month').value;
+    const year = document.getElementById('year').value;
+
+    const birthDate = new Date(year, month - 1, day);
+    const today = new Date();
+
+    let ageYears = today.getFullYear() - birthDate.getFullYear();
+    let ageMonths = today.getMonth() - birthDate.getMonth();
+    let ageDays = today.getDate() - birthDate.getDate();
+
+}
+
+
+document.getElementById('years').innerText = ageYears;
+document.getElementById('months').innerText = ageMonths;
+document.getElementById('days').innerText = ageDays;
